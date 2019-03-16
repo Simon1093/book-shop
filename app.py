d822de22ba0ad2a6e80559b9232ec9f9d0eb3d7c
Nik <n.skubak@storytelling-software.com>
2019-03-16 15:16:44 +0300

    Changed templates (managed design)

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from datetime import datetime
+from bson import ObjectId
 
-from infrastructure import EnvironmentManager
+from flask import Flask, render_template, redirect, request
+
+from repositories import PasteBinRepository
+from models import PasteBin
 
 
 app = Flask('pastebin')
+
+pastebin_repo = PasteBinRepository()
 
 
 @app.route('/')
@@ -11,10 +17,32 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/secret')
-def secret():
-    em = EnvironmentManager()
-    return em.get('TEST_SECRET')
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return redirect('/')
+    attrs = {}
+    title = request.form.get('title')
+    attrs['title'] = title if title != '' else 'No title'
+    attrs['data'] = request.form.get('data')
+    attrs['datetime'] = datetime.now()
+    if len(attrs['data']) == 0:
+        return redirect('/')
+    pastebin = PasteBin(attrs)
+    result = pastebin_repo.insert_one(pastebin)
+    return redirect(f'/pastebin/{result}')
+
+
+@app.route('/pastebin/<pastebin_id>')
+def pastebin(pastebin_id):
+    try:
+        pastebin_id = ObjectId(pastebin_id)
+    except:
+        return redirect('/')
+    result = pastebin_repo.find_one({'_id': pastebin_id})
+    if result is None:
+        return redirect('/')
+    return render_template('pastebin.html', result=result.to_json())
 
 
 if __name__ == '__main__':
